2a7b9f39fae94ba2a9fd28dd2134b46c6a3fa464
RaffaeleRiccardelli <raf.riccardelli@gmail.com>
2020-01-30 17:33:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -37,14 +37,13 @@ var users = [];
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 app.use(flash());
-// app.use(session({
-//   secret: process.env.SESSION_SECRET,
-//   //resave our variables if nothing is changed
-//   resave: false,
-//   //save empty value if set on true
-//   saveUninitialized: false
-//
-// }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  //resave our variables if nothing is changed
+  resave: false,
+  //save empty value if set on true
+  saveUninitialized: false
+}));
 
 // app.use(passport.initialize());
 // app.use(passport.session());
