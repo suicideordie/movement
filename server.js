7545a872fda12d57d1dc501d5a830c11a0366584
RaffaeleRiccardelli <raf.riccardelli@gmail.com>
2020-01-31 12:33:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ initializePassport(
 var users = [];
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: false}));
-app.use(flash());
+// app.use(express.urlencoded({extended: false}));
+// app.use(flash());
 
 // Create a session for developing and debugging
 // app.use(session({
