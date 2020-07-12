e6958d9a3dd067683cd10246e6f0be4c5bfe9140
Stefano-Ferrari <36973253+Stefano-Ferrari@users.noreply.github.com>
2020-07-12 15:02:59 +0200

    Database implementation complete

--- views/sketch_blog.js
+++ views/sketch_blog.js
@@ -2,13 +2,20 @@ var socket;
 var database;
 var logoutBtn;
 var postbutton;
-var container;
+// var container;
 var post;
 var user;
+var userData;
 var date;
+var userData;
 var author;
 var text;
+var textData;
+var timestamp;
+var timestampData;
 var lastMSG = null;
+var arrayMSG;
+var contatore = 0;
 
 function setup() {
   noCanvas();
@@ -35,7 +42,15 @@ function setup() {
   database = firebase.database();
   console.log(database);
 
-  keyCheck();
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+      console.log(firebaseUser);
+    } else {
+      window.location.href = "/login.html";
+    }
+  });
+
+  // keyCheck();
 
   logoutBtn = document.getElementById("logoutBtn");
 
@@ -43,40 +58,32 @@ function setup() {
     firebase.auth().signOut();
   });
 
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser){
-      console.log(firebaseUser);
-    } else {
-      window.location.href = "/login.html";
-    }
-  });
+  showAllMsg();
 
   postbutton = document.getElementById("postbutton");
 
   postbutton.addEventListener("click", e => {
+    console.log(document.getElementById("text").value);
+    if (document.getElementById("text").value != "") { //Evita l'invio di messaggi vuoti
+      text = document.getElementById("text").value;
 
-    text = document.getElementById("text").value; //TODO evitare messaggi vuoti
-
-    console.log(lastMSG);
-    //TODO: creare array: testo+autore+data
-    var ref = database.ref("/messaggi");
-    ref.on('value', gotMSG, errMSG);
-    var insertMSG = ref.child(lastMSG.toString()).set(text); //TODO sostituire text con array
-
-    //TODO creare array, leggere array, scrivere dati array nei messaggi e riscrivere ogni volta la "cronologia" dei messaggi
-
-
-    container = createDiv();
-    container.addClass("post");
-    container.parent("blogbox");
-    post = createP(text);
-    post.addClass("postText");
-    post.parent(container);
-    author = createP("Written by " + user + " on " + date);
-    author.addClass("postAuthor");
-    author.parent(container);
-
-    document.getElementById("text").value = "";
+      console.log(lastMSG);
+      var usermail = firebase.auth().currentUser.email;
+      var tempArray = usermail.split('@');
+      user = tempArray[0];
+      var h = hour();
+      var min = minute();
+      var d = day();
+      var m = month();
+      var y = year();
+      timestamp = d + "." + m + "." + y + " " + h + ":" + min;
+      arrayMSG = [text, user, timestamp];
+      var ref = database.ref("messaggi");
+      var readMsgListener = ref.on('value', gotMSG, errMSG);
+      var insertMSG = ref.child(lastMSG.toString()).set(arrayMSG);
+      // window.location.reload();
+      //TODO scrivere dati array nei messaggi e riscrivere ogni volta la "cronologia" dei messaggi
+    }
   });
 
   function gotMSG(data) {
@@ -89,21 +96,98 @@ function setup() {
   }
 }
 
-function keyCheck(){
-  var ref = database.ref("/messaggi");
-  ref.on('value', gotMSG, errMSG);
+// function keyCheck(){
+//   var ref = database.ref("/messaggi");
+//   var keyCheckListener = ref.on('value', checkMSG, errorMSG);
+// }
+//   //retrieve last key to increase database
+//   function checkMSG(data) {
+//     if (lastMSG == null) {
+//       lastMSG = 0;
+//     } else {
+//       // console.log(Object.keys(data.val()));
+//       lastMSG = Object.keys(data.val()).length;
+//     }
+//
+//   }
+//
+//   function errorMSG(err) {
+//     console.log('Error: ' + err);
+//   }
 
-  //retrieve last key to increase database
-  function gotMSG(data) {
-    if (lastMSG == null) {
-      lastMSG = 0;
-    } else {
-      // console.log(Object.keys(data.val()));
-      lastMSG = Object.keys(data.val()).length;
+
+
+function showAllMsg() {
+  var ref = database.ref();
+  ref.on('value', gotData, errData);
+}
+
+function gotData(data) {
+  lastMSG = Object.keys(data.val()["messaggi"]).length;
+  console.log(lastMSG);
+
+  if (contatore == 0) {
+    for (var i = 0; i < lastMSG; i++) {
+      textData = data.val()["messaggi"][i][0];
+      userData = data.val()["messaggi"][i][1];
+      timestampData = data.val()["messaggi"][i][2];
+
+      var container = document.createElement("div");
+      container.classList.add("post");
+      container.id = "post" + i;
+      var blogbox = document.getElementById("blogbox");
+      if (i == 0) {
+        blogbox.appendChild(container);
+      } else {
+        var prev = document.getElementById("post" + (i - 1));
+        blogbox.insertBefore(container, prev);
+      }
+
+      post = createP(textData);
+      post.addClass("postText");
+      post.parent(container);
+      author = createP("Written by " + userData + " on " + timestampData);
+      author.addClass("postAuthor");
+      author.parent(container);
     }
-  }
 
-  function errMSG(err) {
-    console.log('Error: ' + err);
+    contatore++;
+
+  } else {
+    for (var i = 0; i < lastMSG-1; i++) {
+      console.log(i);
+      var div = document.getElementById("post" + i);
+      div.remove();
+    }
+
+    for (var i = 0; i < lastMSG; i++) {
+      textData = data.val()["messaggi"][i][0];
+      userData = data.val()["messaggi"][i][1];
+      timestampData = data.val()["messaggi"][i][2];
+
+      var container = document.createElement("div");
+      container.classList.add("post");
+      container.id = "post" + i;
+      var blogbox = document.getElementById("blogbox");
+      if (i == 0) {
+        blogbox.appendChild(container);
+      } else {
+        var prev = document.getElementById("post" + (i - 1));
+        blogbox.insertBefore(container, prev);
+      }
+
+      post = createP(textData);
+      post.addClass("postText");
+      post.parent(container);
+      author = createP("Written by " + userData + " on " + timestampData);
+      author.addClass("postAuthor");
+      author.parent(container);
+    }
+    document.getElementById("text").value = "";
   }
+}
+
+function errData(err) {
+  console.log('Error: ' + err);
+
 }
